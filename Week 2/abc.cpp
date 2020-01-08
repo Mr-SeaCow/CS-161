72aa65bb813ca5e8fef7f3105f6d5e1a2ada91fb
Matthew Van Vleet <mbvanvl@gmail.com>
2020-01-08 14:18:29 -0800

    Finally It?

---
+++ Week 2/abc.cpp
@@ -0,0 +1,28 @@
+/*
+File:           ip1a_elir_matthewv_list.cpp
+Description:    Display animals
+Author:         Matthew Van Vleet and Eli Rankin
+Date:           1/7/20
+Version:        C++ 14
+*/
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    string variable;
+    cout << "Animals" << endl << endl;
+    cout << "Koala" << endl;
+    cout << "Kangaroo" << endl;
+    cout << "Emu" << endl;
+    cout << "Huntsman Spider" << endl;
+    cout << "Wallaby" << endl
+         << "Wombat" << endl
+         << "Crocodile" << endl
+         << "Platypus" << endl
+         << "Echidna" << endl
+         << "Dingo";
+    return 0;
+}
---
+++ Week 2/abcd.cpp
@@ -0,0 +1,19 @@
+/*
+File:           ip1b_elir_matthewv_diagram.cpp
+Description:    A program that uses escape sequence to output
+                a simple diagram.
+Author:         Matthew Van Vleet and Eli Rankin
+Date:           1/7/20
+Version:        C++ 14
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "_________\n|\t|\n| V | V |\n|   |_  |\n|\t|\n| ----- |\n|_______|";
+
+    return 0;
+}
+
